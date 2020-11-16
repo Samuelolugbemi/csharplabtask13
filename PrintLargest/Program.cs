fc88756cbac8b379b98791fced326c1fd5ea0644
Olugbemi Samuel <olugbemisamuel600@gmail.com>
2020-11-16 08:02:09 +0100

    First Comiit

---
+++ PrintLargest/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Func< int[], int> largestNumber = (num) => { 
+                int max = num[0];
+                for (int i = 0; i < num.Length; i++)
+                {
+                    if(num[i] > max)
+                    {
+                        max = num[i];
+                    }
+                }
+                return max;
+            };
+            int[] arr = {1, 2, 3, 4, 5, 6};
+            Console.WriteLine(largestNumber(arr));
+        }
+    }
+}
---
+++ SquareArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Question5
+{
+    public delegate void Numbers(int[] num);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              Numbers squareArray = (num) =>
+            {
+                for (int i = 0; i < num.Length; i++)
+                {
+                    num[i] = (int)(Math.Pow(num[i], 2));
+                }
+                foreach (var item in num)
+                {
+                    Console.Write(item + ",");
+                }
+            };
+            int[] arr= {12,13,2,5,7};
+            squareArray(arr);
+        }
+    }
+}
+// private static int[] squareArray(int[] array)
+// {
+//     int[] result = new int[array.Length];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         result[i] = (int)Math.Pow(array[i], 2);
+//     }
+//     return result;
+// }
